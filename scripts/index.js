7a9cdcea3efd732dedea203cea0b65029b5951a7
Alex Karpov <aezred2@yandex.ru>
2022-01-24 23:47:22 +0300

    fix: fix all errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let popupOpen = document.querySelector('.profile__redaction-button');
-console.log(popupOpen); //нажали на кнопку править
+
 let popup = document.querySelector('.popup'); //нашли элемент, которому будем добавлять/удалять стили
-console.log(popup);
+
 let popupClose = document.querySelector('.popup__button-close') //нашли крестик
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
@@ -12,8 +12,7 @@ console.log(nameInput.value);
 let jobInput = document.querySelector('.popup__input_field_activity')// Воспользуйтесь инструментом .querySelector()
 
 popupOpen.addEventListener('click', () => popup.classList.add('popup_opened'));
-// popupClose.addEventListener('click', popupCloser);
-// popupSubmit.addEventListener('click', () => popup.classList.remove('popup_opened'));
+popupClose.addEventListener('click', popupCloser);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__admin') // Воспользуйтесь методом querySelector
@@ -22,28 +21,14 @@ function popupCloser() {
   document.querySelector('.popup').classList.remove('popup_opened')
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-  // Получите значение полей jobInput и nameInput из свойства value
-
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-
   profileTitle.textContent = nameInput.value
   profileSubtitle.textContent = document.querySelector('.popup__input_field_activity').value;
   popupCloser();
-  // Вставьте новые значения с помощью textContent
-  // document.querySelector('.popup__submit').addEventListener('click', () => popup.classList.remove('popup_opened'));
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-
 
 function preLoadText() {
   profileTitle.textContent = 'Жак-Ив Кусто'
