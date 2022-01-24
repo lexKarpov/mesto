fc204bb5a9eccd3fc335f7335c387b2bbf620ad4
Alex Karpov <aezred2@yandex.ru>
2022-01-24 20:19:46 +0300

    fix: fix errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,26 @@
-let popupOpen = document.querySelector('.profile__setting-image') //нажали на кнопку править
+let popupOpen = document.querySelector('.profile__redaction-button');
+console.log(popupOpen); //нажали на кнопку править
 let popup = document.querySelector('.popup'); //нашли элемент, которому будем добавлять/удалять стили
+console.log(popup);
 let popupClose = document.querySelector('.popup__button-close') //нашли крестик
-let popupSubmit = document.querySelector('.popup__submit')
-let nameNewText = '';
-let jobNewText = '';
-
-let ptofileTitle = document.querySelector('.profile__title');
+let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_field_name')// Воспользуйтесь инструментом .querySelector()
+console.log(nameInput.value);
+let jobInput = document.querySelector('.popup__input_field_activity')// Воспользуйтесь инструментом .querySelector()
 
 popupOpen.addEventListener('click', () => popup.classList.add('popup_opened'));
-popupClose.addEventListener('click', () => popup.classList.remove('popup_opened'));
-popupSubmit.addEventListener('click', () => popup.classList.remove('popup_opened'));
+// popupClose.addEventListener('click', popupCloser);
+// popupSubmit.addEventListener('click', () => popup.classList.remove('popup_opened'));
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__admin') // Воспользуйтесь методом querySelector
 
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name')// Воспользуйтесь инструментом .querySelector()
-console.log(nameInput.value);
-let jobInput = document.querySelector('.popup__activity')// Воспользуйтесь инструментом .querySelector()
+function popupCloser() {
+  document.querySelector('.popup').classList.remove('popup_opened')
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -28,15 +29,24 @@ function formSubmitHandler(evt) {
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
   // Получите значение полей jobInput и nameInput из свойства value
-  nameNewText = nameInput.value;
-  jobNewText = jobInput.value;
+
 
   // Выберите элементы, куда должны быть вставлены значения полей
-  ptofileTitle.textContent = nameNewText;
-  profileSubtitle.textContent = jobNewText;
+
+  profileTitle.textContent = nameInput.value
+  profileSubtitle.textContent = document.querySelector('.popup__input_field_activity').value;
+  popupCloser();
   // Вставьте новые значения с помощью textContent
+  // document.querySelector('.popup__submit').addEventListener('click', () => popup.classList.remove('popup_opened'));
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+function preLoadText() {
+  profileTitle.textContent = 'Жак-Ив Кусто'
+  profileSubtitle.textContent = 'Исследователь океана'
+}
+preLoadText()
